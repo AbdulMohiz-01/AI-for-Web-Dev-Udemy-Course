7f80c550c3eb357d4d890122d0a4bf2a078a4d23
Abdul Mohiz <111192201+AbdulMohiz-01@users.noreply.github.com>
2023-08-16 22:20:54 +0500

    Update index.js

--- Embeddings/yt-singlestore/index.js
+++ Embeddings/yt-singlestore/index.js
@@ -4,7 +4,6 @@ const openAiHeaders = {
     'Content-Type': 'application/json',
     'Authorization': `Bearer `,
 }
-// sk-h8cTgr1ukuvQM9uhbYlwT3BlbkFJ60Gf1q4w2TyqsHU2KcLV7
 
 async function getOpenAiResponse(prompt) {
     const response = await fetch('https://api.openai.com/v1/embeddings', {
