2ed56cacc075db7ea3485eaee3297a7db1df9948
Abdul Mohiz <FA20-BCS-001@cuiatd.edu.pk>
2023-08-16 22:13:04 +0500

    all ok

--- Embeddings/yt-singlestore/index.js
+++ Embeddings/yt-singlestore/index.js
@@ -6,7 +6,7 @@ const openAiHeaders = {
 }
 // sk-h8cTgr1ukuvQM9uhbYlwT3BlbkFJ60Gf1q4w2TyqsHU2KcLV7
 
-async function getOpenAiesponse(prompt) {
+async function getOpenAiResponse(prompt) {
     const response = await fetch('https://api.openai.com/v1/embeddings', {
         method: 'POST',
         headers: openAiHeaders,
